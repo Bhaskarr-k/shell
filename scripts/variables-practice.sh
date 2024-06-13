780a317cfada5655a22e7624c5be866f492aa297
Bhaskar <bhaskarkotakonda123@gmail.com>
2024-06-13 09:06:30 +0530

    variables

---
+++ scripts/variables-practice.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+name=basu
+name1=bhaskar
+course=devops
+
+echo $name
+echo $name1
+echo $course
+
+echo "i am vary         exicted to learn about devops"
+
+
+echo'git is version ocntrol           system'
+
+
+echo ${name} try to learn ${course}
+
+address="chennai pileru                    tirupati"
+
+location='bengaluru                hyderabad'
+
+echo $address
+
+echo $location
+
+
+echo $@
+
+echo $#
